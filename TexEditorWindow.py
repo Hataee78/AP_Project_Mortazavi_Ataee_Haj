db4a601a33b797690d062cdde8f18bd32ae7877e
hossein <hataee251078@gmail.com>
2022-06-30 12:12:07 +0430

    Text editor

--- TexEditorWindow.py
+++ TexEditorWindow.py
@@ -1,7 +1,8 @@
 from designe import Ui_TextEditor
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QFileDialog ,QMessageBox
-from PyQt5.QtPrintSupport import QPrinter, QPrintDialog,QPrintPreviewDialog
+from PyQt5.QtWidgets import QFileDialog, QMessageBox
+from PyQt5.QtPrintSupport import QPrinter, QPrintDialog, QPrintPreviewDialog
+
 
 class EditorWindow(QtWidgets.QMainWindow, Ui_TextEditor):
     def __init__(self) -> None:
@@ -13,50 +14,45 @@ class EditorWindow(QtWidgets.QMainWindow, Ui_TextEditor):
         self.actionSave.triggered.connect(self.fileSave)
         self.actionPrint.triggered.connect(self.printfile)
         self.actionPrint_Preview.triggered.connect(self.printPreview)
-        
 
         self.show()
 
     def fileNew(self):
         self.textEdit.clear()
-    
+
     def openFile(self):
-        filename = QFileDialog.getOpenFileName(self,'Open File','/home')
-        
+        filename = QFileDialog.getOpenFileName(self, 'Open File', "/home")
+
         if filename[0]:
-            f= open(filename[0],'r')
-            
+            f = open(filename[0], 'r')
             with f:
                 data = f.read()
-                self.textEdit.setText(data)    
-                
+                self.textEdit.setText(data)
+
     def fileSave(self):
-        filename = QFileDialog.getSaveFileName(self,'Save File')      
+        filename = QFileDialog.getSaveFileName(self, 'Save File')
         if filename[0]:
-            f=open(filename[0],'w') 
-            
+            f = open(filename[0], 'w')
+
             with f:
                 text = self.textEdit.toPlainText()
                 f.write(text)
-                
-                QMessageBox.about(self,"Save File","File Saved Successful")     
-    
-    
+
+                QMessageBox.about(self, "Save File", "File Saved Succuessful")
+
     def printfile(self):
         printer = QPrinter(QPrinter.HighResolution)
-        dialog = QPrintDialog(printer,self)
-        
-        if dialog.exec_()== QPrintDialog.Accepted:
+        dialog = QPrintDialog(printer, self)
+
+        if dialog.exec_() == QPrintDialog.accepted:
             self.textEdit.print_(printer)
-                       
-                       
+
     def printPreview(self):
         printer = QPrinter(QPrinter.HighResolution)
-        previewDialog = QPrintPreviewDialog(printer,self)
-        previewDialog.paintRequested.connect(self.printPreview)
+        previewDialog = QPrintPreviewDialog(printer, self)
+        previewDialog.paintRequested.connect(self.printPreview2)
         previewDialog.exec_()
-        
-        
-    def printPreview2(self,printer):
-        self.textEdit.print_(printer)     
-                        
+
+    def printPreview2(self, printer):
+        self.textEdit.print_(printer)
+
